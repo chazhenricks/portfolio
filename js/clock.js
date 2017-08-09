85f2a5af31fc08b329f0bd38de9c7a9e5f5e9d96
Chaz Henricks <chazhenricks@gmail.com>
2017-08-09 13:18:43 -0500

    CLOCK WORKS

--- js/clock.js
+++ js/clock.js
@@ -1,20 +1,17 @@
+"use strict";
+
+var clockInfo = document.querySelector('.clock_info');
+var clockHTML = `<div class="flip hour-tens"></div>
+    <div class="flip hour-ones"></div>
+    <div class="flip minute-tens"></div>
+    <div class="flip minute-ones"></div>`;
+clockInfo.innerHTML = clockHTML;
+
+
 var hourTens = document.querySelector('.hour-tens');
 var hourOnes = document.querySelector('.hour-ones');
 var minuteTens = document.querySelector('.minute-tens');
 var minuteOnes = document.querySelector('.minute-ones');
-var clockInfo = document.querySelector('.clock_info');
-
-
-var clockHTML = `<div class="flip-clock-left">
-    <div class="flip hour-tens"></div>
-    <div class="flip hour-ones"></div>
-    </div>
-    <div class="flip-clock-right">
-    <div class="flip minute-tens"></div>
-    <div class="flip minute-ones"></div>
-</div>`;
-
-clockInfo.innerHTML = clockHTML;
 
 function getHour(now){
     var hour = now.getHours();
@@ -27,9 +24,9 @@ function getHour(now){
         hourString = "0" + hourString;
     }
 
-    hourArray = hourString.split("");
-    var hourTensNum = `<div class="line"></div><p class="time hourTensNum">${hourArray[0]}</p>`;
-    var hourOnesNum = `<div class="line"></div><p class="time hourOnesNum">${hourArray[1]}</p>`;
+    var hourArray = hourString.split("");
+    var hourTensNum = `<div class="line"></div><div class="time hourTensNum">${hourArray[0]}</div>`;
+    var hourOnesNum = `<div class="line"></div><div class="time hourOnesNum">${hourArray[1]}</div>`;
     hourTens.innerHTML = hourTensNum;
     hourOnes.innerHTML = hourOnesNum;
 }
@@ -41,9 +38,9 @@ function getMinutes(now){
     if (minString.length < 2){
         minString = "0" + minString;
     }
-    minsArray = minString.split("");
-    var minTensNum = `<div class="line"></div><p class="time minTensNum">${minsArray[0]}</p>`;
-    var minOnesNum = `<div class="line"></div><p class="time minOnesNum">${minsArray[1]}</p>`;
+    var minsArray = minString.split("");
+    var minTensNum = `<div class="line"></div><div class="time minTensNum">${minsArray[0]}</div>`;
+    var minOnesNum = `<div class="line"></div><div class="time minOnesNum">${minsArray[1]}</div>`;
     minuteTens.innerHTML = minTensNum;
     minuteOnes.innerHTML = minOnesNum;
 }
