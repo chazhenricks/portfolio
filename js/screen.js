4369e59f7e5c808540be046b570708ac142f9648
Chaz Henricks <chazhenricks@gmail.com>
2017-08-09 16:32:12 -0500

    screen is displaying text

---
+++ js/screen.js
@@ -0,0 +1,119 @@
+"use strict";
+var screenDIV = document.querySelector(".screen_content");
+var screenHTML = `
+    <div class="screen">
+        <div class="screen-content">
+            <ol class="info">
+                <li>
+                    <span class="bracket"><</span>
+                    <span class="code_text">html</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">head</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">meta</span>
+                    <span class="attribute">name</span>
+                    <span class="bracket">=</span> 
+                    <span class="attribute_text">'author'</span>
+                    <span class="attribute"> content</span>
+                    <span class="bracket">=</span> 
+                    <span class="attribute_text">'Chaz Henricks &lt;chazhenricks@gmail.com&gt;'</span>
+                    <span class="code_text">title</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">title</span>
+                    <span class="bracket">></span>
+                    <span class="string_text">Chaz Henricks Portfoloio</span> 
+                    <span class="bracket">< /</span>
+                    <span class="code_text">title</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;
+                    <span class="bracket">< /</span>
+                    <span class="code_text">head</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">body</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">h1</span>
+                    <span class="bracket">></span>
+                    <span class="string_text">Here are some personal projects</span>
+                    <span class="bracket">< /</span>
+                    <span class="code_text">h1</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">ul</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">li</span>
+                    <span class="bracket">></span>
+                    <a href="" class="link_text">Play Drums!</a>
+                    <span class="bracket">< /</span>
+                    <span class="code_text">li</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">li</span>
+                    <span class="bracket">></span>
+                    <a href="" class="link_text">Shows Around</a>
+                    <span class="bracket">< /</span>
+                    <span class="code_text">li</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bracket"><</span>
+                    <span class="code_text">li</span>
+                    <span class="bracket">></span>
+                    <a href="" class="link_text">Backend Capstone</a>
+                    <span class="bracket">< /</span>
+                    <span class="code_text">li</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <span class="bracket">< /</span>
+                    <span class="code_text">ul</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    &nbsp;&nbsp;
+                    <span class="bracket">< /</span>
+                    <span class="code_text">body</span>
+                    <span class="bracket">></span>
+                </li>
+                <li>
+                    <span class="bracket">< /</span>
+                    <span class="code_text">html</span>
+                    <span class="bracket">></span>
+                </li>
+            </ol>  
+        </div>
+    </div>`;
+screenDIV.innerHTML = screenHTML;
