d542a5c52e19a58ae1b4a0e55f55f7fa9702635b
Chaz Henricks <chazhenricks@gmail.com>
2017-08-16 09:29:15 -0500

    back to smashmouth only video

--- js/youtube.js
+++ js/youtube.js
@@ -1,35 +1,30 @@
 "use strict";    
 
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-var player;
-$(".speaker_left_div").click(newPlayer('L_jWHffIx5E'));
-$(".speaker_right_div").click(newPlayer('dQw4w9WgXcQ'));
-
-function newPlayer(vidID){
-    $("#player").removeClass("hidden");
-    
-}
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      height: '390',
-      width: '640',
-      videoId: 'M7lc1UVf-VE',
-      events: {
-        'onStateChange': onPlayerStateChange
-      }
+$(".speaker_left_div").click(function(){
+        $("#player").removeClass("hidden");
+        player = new YT.Player('player', {
+            width : '427px',
+            height : '230px',
+            videoId : 'L_jWHffIx5E',
+            playerVars: { 'autoplay': 1 },
+            events : {
+                'onReady' : onPlayerReady,
+                'onStateChange' : onPlayerStateChange
+            }
+        });
     });
-  }
-function onPlayerStateChange(event) {
-    if(event.data == YT.PlayerState.ENDED) {
-        player.destroy();
+
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    var player;
+    function onPlayerReady(event) {
+        //event.target.playVideo();
     }
-}
-function stopVideo() {
-    player.stopVideo();
-  }
+    function onPlayerStateChange(event) {
+        if(event.data == YT.PlayerState.ENDED) {
+            player.destroy();
+        }
+    }
